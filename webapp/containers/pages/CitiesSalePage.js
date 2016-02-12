70877d998ec885ba8cc83ac354344a41e13682ff
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-02-12 12:31:08 -0600

    Css Typography styles

--- webapp/containers/pages/CitiesSalePage.js
+++ webapp/containers/pages/CitiesSalePage.js
@@ -7,6 +7,16 @@ import CardHeader from '../../../node_modules/material-ui/lib/card/card-header';
 import CardText from '../../../node_modules/material-ui/lib/card/card-text';
 import House_List from './../../components/HouseSale/House_List.jsx';
 
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import ActionGrade from 'material-ui/lib/svg-icons/action/grade';
+import ActionInfo from 'material-ui/lib/svg-icons/action/info';
+import ContentInbox from 'material-ui/lib/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/lib/svg-icons/content/drafts';
+import ContentSend from 'material-ui/lib/svg-icons/content/send';
+import Divider from 'material-ui/lib/divider';
+import Avatar from 'material-ui/lib/avatar';
+import Colors from 'material-ui/lib/styles/colors';
 import SvLink from '../../components/Shared/SvLink';
 
 import {isomorphicVars} from '../../scripts/isomorphicVars';
@@ -14,7 +24,7 @@ import {isomorphicVars} from '../../scripts/isomorphicVars';
 class CitiesSale extends React.Component {
 
     getChildContext() {
-       return {location:this.props.location};
+        return {location: this.props.location};
     };
 
     static childContextTypes = {
@@ -25,20 +35,26 @@ class CitiesSale extends React.Component {
         var isoVars = isomorphicVars();
         return (
             <div>
-                Houses for SALE
-                <ul >
-                    {this.props.Viewer.Cities.edges.map((city, index)=>
-                        <li key={city.node.name}>
-                            <SvLink url={city.node.name}/>
+                <h1>
+                    Houses for Sale
+                </h1>
 
-                        </li>
-                    )}
-                </ul>
                 <hr/>
-                <SvLink url="hoffman estates">
-                    <img style={{width:200}}
-                         src="https://upload.wikimedia.org/wikipedia/commons/9/9c/Skokie,_Illinois.jpg"/>
-                </SvLink>
+
+                <List
+                    insetSubheader={true}
+                >
+                    {this.props.Viewer.Cities.edges.map((city, index)=>
+                        <ListItem
+                            leftAvatar={<Avatar src="https://upload.wikimedia.org/wikipedia/commons/9/9c/Skokie,_Illinois.jpg"/>}
+                            secondaryText={ <p> <span style={{color: Colors.darkBlack}}  > Median Price: </span> <br/> <span >#Listings: | </span> </p> }
+                            secondaryTextLines={2}
+                            rightIcon={<ActionInfo />} key={city.node.name}>
+                            <SvLink url={city.node.name}/>
+                        </ListItem>
+                    )}
+                </List>
+                <hr/>
             </div>
         );
     }
