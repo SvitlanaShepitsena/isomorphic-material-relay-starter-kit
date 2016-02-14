2365763204618a82e8691e65b8414acef1085657
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-02-14 15:31:10 -0600

    House Type

--- data/da/House.js
+++ data/da/House.js
@@ -1,10 +1,12 @@
 import {
     House_list_get,
-    House_list_get_city
+    House_list_get_city,
+    House_get
 
 } from '../da_cassandra/House'
 
 export {
+    House_get,
     House_list_get,
     House_list_get_city,
 };
