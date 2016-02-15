feb53926081b5b36d50efffd20cd898dc3bbc5b1
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-02-15 10:27:53 -0600

    Master comit

--- data/da_cassandra/House.js
+++ data/da_cassandra/House.js
@@ -8,33 +8,14 @@ export function House_list_get() {
 
     return runQuery(House, cqlText, cqlParams);
 }
-export function House_list_get_city(city, zipType) {
-    console.log(city);
-    console.log(zipType);
-    let cqlParams;
-    let cqlText;
-    if (!zipType) {
-        cqlText = 'SELECT * FROM "house" where city = ? ALLOW FILTERING;';
-        cqlParams = [city];
-    } else {
-        if (zipType.match(/\d+/g)) {
-            console.log('House.js - line: 20');
-            cqlText = 'SELECT * FROM "house" where city = ? and zip = ? ALLOW FILTERING;';
-
-            cqlParams = [city, zipType];
-            console.log(cqlParams);
-        } else {
-            cqlText = 'SELECT * FROM "house" where city = ? and type = ? ALLOW FILTERING;';
-            cqlParams = [city, zipType];
-
-        }
-    }
+export function House_list_get_city(city) {
+    let cqlText = 'SELECT * FROM "house" where city = ? ALLOW FILTERING;';
+    let cqlParams = [city];
 
     return runQuery(House, cqlText, cqlParams);
 }
 
 export function House_get(id) {
-    console.log(id);
     const cqlText = 'SELECT * FROM "house" WHERE id = ? ALLOW FILTERING;';
     const cqlParams = [id];
 
