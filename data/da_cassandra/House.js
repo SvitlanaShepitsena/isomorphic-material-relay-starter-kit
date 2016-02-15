54836dd75d97e403321d1bfcdcb74ab2baa54d9a
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-02-15 10:11:13 -0600

    add files

--- data/da_cassandra/House.js
+++ data/da_cassandra/House.js
@@ -8,14 +8,33 @@ export function House_list_get() {
 
     return runQuery(House, cqlText, cqlParams);
 }
-export function House_list_get_city(city) {
-    let cqlText = 'SELECT * FROM "house" where city = ? ALLOW FILTERING;';
-    let cqlParams = [city];
+export function House_list_get_city(city, zipType) {
+    console.log(city);
+    console.log(zipType);
+    let cqlParams;
+    let cqlText;
+    if (!zipType) {
+        cqlText = 'SELECT * FROM "house" where city = ? ALLOW FILTERING;';
+        cqlParams = [city];
+    } else {
+        if (zipType.match(/\d+/g)) {
+            console.log('House.js - line: 20');
+            cqlText = 'SELECT * FROM "house" where city = ? and zip = ? ALLOW FILTERING;';
+
+            cqlParams = [city, zipType];
+            console.log(cqlParams);
+        } else {
+            cqlText = 'SELECT * FROM "house" where city = ? and type = ? ALLOW FILTERING;';
+            cqlParams = [city, zipType];
+
+        }
+    }
 
     return runQuery(House, cqlText, cqlParams);
 }
 
 export function House_get(id) {
+    console.log(id);
     const cqlText = 'SELECT * FROM "house" WHERE id = ? ALLOW FILTERING;';
     const cqlParams = [id];
 
