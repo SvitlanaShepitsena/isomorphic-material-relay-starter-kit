c74ebcf4ab746bc647033ff8eb9d720c75b90ab0
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-02-29 15:45:12 -0600

    Pagination Design

--- webapp/components/Pagination/Pagination.js
+++ webapp/components/Pagination/Pagination.js
@@ -1,11 +1,10 @@
 import React, {PropTypes} from 'react';
-import Relay from 'react-relay';
 import {Link} from 'react-router'
-
+/*=materialUi*/
 import FlatButton from 'material-ui/lib/flat-button';
-import _ from 'lodash';
 
-/*Components*/
+/*=styles*/
+import styles from './Pagination.less';
 
 class Pagination extends React.Component {
     static contextTypes = {location: PropTypes.object.isRequired};
@@ -19,16 +18,22 @@ class Pagination extends React.Component {
         const {lastPage, pageInfo} = this.props;
         const currentPage = Number(this.context.location.query.page || 1);
         return (
-            <div>
-                {lastPage !== currentPage &&
-                <Link
-                    to={{ pathname: this.context.location.pathname, query: { page: currentPage+1,after:pageInfo.startCursor} }}>
-                    <FlatButton label="Next" secondary={true}/>
-                </Link>
-                }
+            <div className={styles.container}>
                 {currentPage > 1 &&
-                <div><Link
-                    to={{ pathname: this.context.location.pathname, query: { page: currentPage-1,before:pageInfo.endCursor} }}>Prev</Link>
+                <div>
+                    <Link
+                        to={{ pathname: this.context.location.pathname, query: { page: currentPage-1,before:pageInfo.endCursor} }}>
+                        <FlatButton label="Previous"
+                                    secondary={true}/>
+                    </Link>
+                </div>
+                }
+                {lastPage !== currentPage &&
+                <div>
+                    <Link
+                        to={{ pathname: this.context.location.pathname, query: { page: currentPage+1,after:pageInfo.startCursor} }}>
+                        <FlatButton label="Next" secondary={true}/>
+                    </Link>
                 </div>
                 }
             </div>
