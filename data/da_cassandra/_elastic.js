2d063033e5bdfb0af66cbfa910ba4b63e6f2704f
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-03-03 09:41:00 -0600

    Andriy Rehabilation

--- data/da_cassandra/_elastic.js
+++ data/da_cassandra/_elastic.js
@@ -1,77 +1,63 @@
 import elasticsearch from 'elasticsearch';
 
-
 var client = new elasticsearch.Client({
     host: 'localhost:9200',
     log: 'trace',
-    apiVersion:'2.2'
+    apiVersion: '2.2'
 });
 
-
-function ensureNoErrorOrReport( qText, qVar, err, reject )
-{
-  if( err )
-  {
-
-
-
-
-
-    reject( err ); // Because terrisgit said so
-  }
-  else
-    return true;
+function ensureNoErrorOrReport(qText, qVar, err, reject) {
+    if (err) {
+        reject(err); // Because terrisgit said so
+    }
+    else
+        return true;
 }
 
-export function runQuery( objectPrototype, qText, qVar )
-{
-  //
-  return new Promise( ( resolve, reject ) =>
-  {
-    client.execute( qText, qVar, {prepare: true}, ( err, result ) => { if( ensureNoErrorOrReport( qText, qVar, err, reject ) )
-    {
-      const resultAsObjects = [ ];
-      const rowCount = result.rowLength;
-      for( let ixRow = 0 ; ixRow < rowCount ; ixRow++ )
-      {
-        let row = result.rows[ ixRow ];
-        resultAsObjects.push( new objectPrototype( row ) );
-      }
-      //
-      resolve( resultAsObjects );
-    } } );
-  } );
+export function runQuery(objectPrototype, qText, qVar) {
+    //
+    return new Promise((resolve, reject) => {
+        client.execute(qText, qVar, {prepare: true}, (err, result) => {
+            if (ensureNoErrorOrReport(qText, qVar, err, reject)) {
+                const resultAsObjects = [];
+                const rowCount = result.rowLength;
+                for (let ixRow = 0; ixRow < rowCount; ixRow++) {
+                    let row = result.rows[ixRow];
+                    resultAsObjects.push(new objectPrototype(row));
+                }
+                //
+                resolve(resultAsObjects);
+            }
+        });
+    });
 }
 
-export function runQueryOneResult( objectPrototype, qText, qVar )
-{
-  //
-  return new Promise( ( resolve, reject ) =>
-  {
-    client.execute( qText, qVar, {prepare: true}, ( err, result ) => { if( ensureNoErrorOrReport( qText, qVar, err, reject ) )
-    {
-      //
-      if( result.rowLength > 0 )
-      {
-        let row = result.rows[ 0 ];
-        const retObj = new objectPrototype( row );
-        resolve( retObj );
-      }
-      else
-        resolve( null );
-    } } );
-  } );
+export function runQueryOneResult(objectPrototype, qText, qVar) {
+    //
+    return new Promise((resolve, reject) => {
+        client.execute(qText, qVar, {prepare: true}, (err, result) => {
+            if (ensureNoErrorOrReport(qText, qVar, err, reject)) {
+                //
+                if (result.rowLength > 0) {
+                    let row = result.rows[0];
+                    const retObj = new objectPrototype(row);
+                    resolve(retObj);
+                }
+                else
+                    resolve(null);
+            }
+        });
+    });
 }
 
-export function runQueryNoResult( qText, qVar )
-{
-  //
-  return new Promise( ( resolve, reject ) =>
-  {
-    client.execute( qText, qVar, {prepare: true}, ( err ) => { if( ensureNoErrorOrReport( qText, qVar, err, reject ) )
-    {
-      //
-      resolve( );
-    } } );
-  } );
+export function runQueryNoResult(qText, qVar) {
+    //
+    return new Promise((resolve, reject) => {
+        client.execute(qText, qVar, {prepare: true}, (err) => {
+            if (ensureNoErrorOrReport(qText, qVar, err, reject)) {
+                //
+                resolve();
+            }
+        });
+    });
 }
