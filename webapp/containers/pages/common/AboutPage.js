f7da85141039c5924d16191a94cd9186b0a97df5
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-03-08 12:18:18 -0600

    add files

--- webapp/containers/pages/common/AboutPage.js
+++ webapp/containers/pages/common/AboutPage.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Helmet from "react-helmet";
+import settings from '../../../settings/settings.js';
+/*=Components*/
 import AboutContent from '../../../components/AppViews/About/AboutContent.js';
 
 class AboutPage extends React.Component {
+
+    pageHelmet() {
+        let og = settings.ogProps.aboutPage;
+        const url = og.url;
+        const title = og.title;
+        const description = og.description;
+        const image = settings.ogProps.fbImage;
+        return (
+            <Helmet
+                title={title}
+                meta={[
+                    {"name": "url", "content": `${url}`},
+                    {"name": "description", "content": `${description}`},
+                    {"name": "image", "content": `${image}`},
+
+                    {"property": "og:url", "content": `${url}`},
+                    {"property": "og:title", "content": `${title}`},
+                    {"property": "og:description", "content": `${description}`},
+                    {"property": "og:image", "content": `${image}`}
+                ]}
+            />
+        );
+    };
+
     render() {
         return (
             <div>
+                {this.pageHelmet()}
                 <AboutContent/>
             </div>
         );
