1425cb100432b53de14795bb44012f19f84aa782
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-03-18 11:31:08 -0500

    ZipType Errors fixed

--- webapp/containers/pages/city/CityPage.js
+++ webapp/containers/pages/city/CityPage.js
@@ -104,7 +104,7 @@ class CityPage extends React.Component {
         let houses = newHouses.length;
         const cityFormatted = urlToText(this.props.params.city);
 
-        const pageTitle = ` Houses for Sale in ${cityFormatted}`;
+        const pageTitle = ` Homes for Sale in ${cityFormatted}`;
         const allTitle = `All ${cityFormatted} homes for sale(${housesCount})`;
 
         return (
