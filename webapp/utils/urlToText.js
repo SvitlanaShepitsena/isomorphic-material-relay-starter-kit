a38f2fb1e2275ab74e89a9204540f92becb130ad
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-04-04 14:39:14 -0500

    duplexes

--- webapp/utils/urlToText.js
+++ webapp/utils/urlToText.js
@@ -3,25 +3,25 @@ import _ from 'lodash';
 export default (text)=> {
     if (text) {
 
-        text = (text.trim().replace(/-+/g, ' ')).toLowerCase();
+        text = _.startCase(text.trim().replace(/-+/g, ' '));
 
-        if (text == 'condominium units') {
+        if (text.toLowerCase() == 'condominium units') {
             text = "Condos";
         }
-        if (text == 'multi family homes') {
+        if (text.toLowerCase() == 'multi family homes') {
             text = "Multi-Family Homes";
         }
-        if (text == 'co op units') {
+        if (text.toLowerCase() == 'co op units') {
             text = "Co-ops";
         }
-        if (text == 'townhouse townhomes') {
+        if (text.toLowerCase() == 'townhouse townhomes') {
             text = "Townhomes";
         }
-        if (text == 'raw lands') {
+        if (text.toLowerCase() == 'raw lands') {
             text = "Raw Land";
         }
 
-        return _.startCase(text);
+        return text;
     }
 }
 
